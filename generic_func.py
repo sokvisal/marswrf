f56cc6e1482aa21fb7d3eed7a4088abbd7f7cc01
Visal Sok <sokvisal@gpc-f102n002-ib0.(none)>
2017-05-11 09:35:09 -0400

    edited init reduction to work on scinet

--- generic_func.py
+++ generic_func.py
@@ -43,13 +43,21 @@ def load_temp(filename, data):
     return ls, temp, press, geo_height
 
 def load_misc(filename, data, var_name):
-    var = var_name + '_AM'
+    var = var_name + '_PHY_AM'
     t_am = data.variables[var][:] # solar long
     
-    var2 = var_name + '_PM'
+    var2 = var_name + '_PHY_PM'
     t_pm = data.variables[var2][:] # solar long
-    
-    return t_am, t_pm
+
+    t_d = t_pm.mean(axis=3) - t_am.mean(axis=3) 
+    t_d_2pa = t_pm[:,42,:,:] - t_am[:,42,:,:]
+
+    return (t_d)/2., t_d_2pa/2.
+
+def load_misc3D(filename, data, var_name):
+    temp = data.variables[var_name][:]
+    ls = data.variables['L_S'][:]
+    return temp, ls
     
     
 def tp(filename, data): # temperature and pressure
@@ -103,67 +111,88 @@ def misc(filename, data): # miscellaneous
     np.save(filename3, geo_height)
     
 def init_reduction(filedir):
+    arg = sys.argv[1]
+    def wrfout():
+	    filepath = filedir + '/wrfout_d01*'
+	    print filepath
+	    if not os.path.exists(filedir+'/reduction'): os.mkdir(filedir+'/reduction')
+	#    print filedir
+	    
+	    for num, i in enumerate(sorted(glob.glob(filepath))):
+		print i
+		if num == 0:
+		    nc_file = i
+		    data = Dataset(nc_file, mode='r')
+		    
+		    ls, temp, press, geoH = load_temp(nc_file, data)
+		else:
+		    nc_file = i
+		    data = Dataset(nc_file, mode='r')
+		    
+		    ls2, temp2, press2, geoH2 = load_temp(nc_file, data)
+		    
+		    ls = np.concatenate((ls, ls2),axis=0)
+		    temp = np.concatenate((temp, temp2),axis=0)
+		    press = np.concatenate((press, press2),axis=0)
+		    geoH = np.concatenate((geoH, geoH2),axis=0)
+	    
+	    filedir2 = i.replace('wrfout','reduction/wrfout')
+	    var_list = ['_ls','_temp','_press','_geoH']
+	    for num, i in enumerate([ls,temp,press,geoH]):
+        	np.save(filedir2 + var_list[num], i)
     
-<<<<<<< HEAD
-    var_name = str(sys.argv[1])
-    
-    filepath = filedir + '/wrfout_d01*'
-=======
-    filepath = filedir + '/wrfout_d01_0001*'
-    print filepath
->>>>>>> 6c6c5441ce6a7abaea0920cfe76f8b265d69a45f
-    if not os.path.exists(filedir+'/reduction'): os.mkdir(filedir+'/reduction')
-#    print filedir
-    
-    for num, i in enumerate(sorted(glob.glob(filepath))):
-        print i
-        if num == 0:
-            nc_file = i
-            data = Dataset(nc_file, mode='r')
-            
-            filedir = i.replace('wrfout','reduction/wrfout')
-            ls, temp, press, geoH = load_temp(filedir, data)
-        else:
-            nc_file = i
-            data = Dataset(nc_file, mode='r')
-            
-            filedir = i.replace('wrfout','reduction/wrfout')
-            ls2, temp2, press2, geoH2 = load_temp(filedir, data)
-            
-            ls = np.concatenate((ls, ls2),axis=0)
-            temp = np.concatenate((temp, temp2),axis=0)
-            press = np.concatenate((press, press2),axis=0)
-            geoH = np.concatenate((geoH, geoH2),axis=0)
-        
-    var_list = ['ls','temp','press','geoH']
-    for num, i in enumerate([ls,temp,press,geoH]):
-        np.save('./test_data/reduction/output_' + var_list[num], i)
-    
-    if var_name:
-        filepath2 = filedir + '/auxhist9*'
-        for num, i in enumerate(sorted(glob.glob(filepath2))):
-            if num == 0:
-                nc_file = i
-                data = Dataset(nc_file, mode='r')
-                
-                filedir = i.replace('auxhist9','reduction/auxhist9')
-                t_am, t_pm = load_misc(filedir, data, var_name)
-            else: 
-                nc_file = i
-                data = Dataset(nc_file, mode='r')
-                
-                filedir = i.replace('auxhist9','reduction/auxhist9')
-                t_am2, t_pm2 = load_misc(filedir, data, var_name)
-                t_am = np.concatenate((t_am, t_am2),axis=0)
-                t_pm = np.concatenate((t_pm, t_pm2),axis=0)
-                
-        var_list = ['t_am','t_pm']
-        for num, i in enumerate([t_am,t_pm]):
-            np.save('./test_data/reduction/output_' + var_list[num], i)
-        
-        
-init_reduction('./../mars/planetWRF-dev-new/WRFV3/test/em_global_mars/')
+    def auxhist9():
+	print 'hi'  
+	print 'cool'
+	filepath2 = filedir + '/auxhist9*'
+	print filepath2
+	for num, i in enumerate(sorted(glob.glob(filepath2))):
+	    print i
+	    if num == 0:
+		nc_file = i
+		data = Dataset(nc_file, mode='r')
+		
+		t_d, t_d_2Pa = load_misc(nc_file, data, 'T' )
+	    else: 
+		nc_file = i
+		data = Dataset(nc_file, mode='r')
+		
+		t_d2, t_d2_2Pa = load_misc(nc_file, data, 'T')
+		t_d = np.concatenate((t_d, t_d2),axis=0)
+		t_d_2Pa = np.concatenate((t_d_2Pa, t_d2_2Pa),axis=0)
+	
+	filedir3 = i.replace('auxhist9','reduction/wrfout')        
+	var_list = ['_t_d','_t_d_2Pa']
+	for num, i in enumerate([t_d,t_d_2Pa]):
+	    np.save(filedir3 + var_list[num], i)
  
+    def auxhist5():    
+	filepath2 = filedir+'/auxhist5*'
+	for num, i in enumerate(sorted(glob.glob(filepath2))):
+	    print i
+	    if num == 0:
+		nc_file = i
+		data = Dataset(nc_file, mode='r')
+
+		psfc, ls_psfc = load_misc3D(nc_file, data, 'PSFC' )
+	    else:
+		nc_file = i
+		data = Dataset(nc_file, mode='r')
+
+		psfc2, ls_psfc2 = load_misc3D(nc_file, data, 'PSFC')
+		psfc = np.concatenate((psfc, psfc2),axis=0)
+		ls_psfc2 = np.concatenate((ls_psfc, ls_psfc2),axis=0)
+
+	filedir3 = i.replace('auxhist5','reduction/wrfout')
+	var_list = ['_psfc','_ls_psfc']
+	for num, i in enumerate([psfc,ls_psfc]):
+	    np.save(filedir3 + var_list[num], i) 
+
+    if arg == 'wrfout': return wrfout()
+    if arg == 'auxhist9': return auxhist9()
+    if arg == 'auxhist5': return auxhist5()
+init_reduction('./../planetWRF/WRFV3/run/new_wbm')
+	 
 def find_ls_idx(ls_arr, ls):
     idx = (np.abs(ls_arr-ls)).argmin() # finding index corresponding to wanted solar long
     assert np.min(np.abs(ls_arr-ls)) < 1, 'Solar Longitude {} not in the file'.format(ls)
