21c13562be3f48f0cb18c6f447cdcdfab6a39960
sokvisal <visal.lesok@gmail.com>
2017-05-25 11:13:52 -0400

    sort of works now for dustL40

--- generic_code.py
+++ generic_code.py
@@ -351,20 +351,20 @@ class hovmoller:
 #                sfc_storm = np.sqrt(np.abs(sfc_storm)*np.sign(sfc_storm))
                 
         if self.rank == 0:
-            sfc_storm = sfc_storm
-#            sfc_storm = sfc_storm.reshape((36, 223, 12)).mean(axis = 2) # smoothing out array
+            sfc_storm = np.concatenate((sfc_storm[:,:5184], np.zeros((36, 29)), sfc_storm[:,5184:]), axis=1) 
+            sfc_storm = sfc_storm.reshape((36, 223, 24)).mean(axis = 2) # smoothing out array
             sfc_storm_normed = sfc_storm/sfc_storm.max(axis=1)[:,np.newaxis]
             np.save('sfc_storm', sfc_storm)
             
             lat = np.linspace(-90,90,36)
-            ls = np.linspace(0,360,5323)
+            ls = np.linspace(0,360,223)
             ls, lat = np.meshgrid(ls, lat)
             
 #            sfc_storm[np.where(sfc_storm>5)] = 0
             
             print ('Saving plot')
             fig, ax = plt.subplots(figsize=(8,6))
-            im = ax.contourf(ls, lat, sfc_storm, cmap='viridis')
+            im = ax.contourf(ls, lat, sfc_storm_normed, cmap='viridis')
             fig.colorbar(im)
             ax.set_ylabel('Latitude')
             ax.set_xlabel('Solar Longitude')
